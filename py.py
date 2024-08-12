953e7bb2b9fb1ff58bc619cff83be637501a2a07
Oybek <.email@example.com>
2024-08-12 21:04:21 +0500

    first

---
+++ py.py
@@ -0,0 +1,52 @@
+# 1
+# a = int(input("Введите число:"))
+# if a / 2 == 0:
+#     print("Число четное")
+# else:
+#     print("Число нечетное")
+# 2
+# A = []  
+# min_ = float('inf')                         
+# a = int(input('Введите целое число...'))   
+# c = int(input('Введите целое число...'))
+# d = int(input('Введите целое число...'))
+# e = int(input('Введите целое число...'))
+# f = int(input('Введите целое число...'))
+# A += a, c, d, e, f
+# for i in A:
+#     if i < min_:                            
+#         min_ = i                            
+# print(f'Минимальное: {min_}' )
+# 3
+# slovo = str(input("What is your word: "))
+# a = slovo[::-1]
+# if slovo == a:
+#   print("yes it is palindrom")
+# else:
+#   print("no  it is not palindrom")
+# 4 
+# n = int(input("Введите число n: "))
+# summa = 0
+# for i in range(1, n + 1):
+#     summa += i  
+# print("Сумма всех чисел от 1 до", n, "равна", summa)
+
+# 5
+# n = int(input("Введите число n: "))
+# for i in range(1, n):
+#     for j in range(1, n):
+#         print("%4d" % (i * j), end='')
+#         print()
+# 6
+# A = []
+# i = int(input("Введите число: "))
+# b = int(input("Введите число: "))
+# c = int(input("Введите число: "))
+# d = int(input("Введите число: "))
+# e = int(input("Введите число: "))
+# A += i, b, c, d, e
+# print(sorted(A))
+
+
+     
+
